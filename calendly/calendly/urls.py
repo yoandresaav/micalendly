a5ee7531b0278a4dcf889b3905d0c46f69252771
Yoandre Saavedra <yoandresaavedra@gmail.com>
2019-08-11 13:17:23 -0500

    add examples pages

--- calendly/calendly/urls.py
+++ calendly/calendly/urls.py
@@ -8,5 +8,10 @@ urlpatterns = [
     url(r'^signup/$', TemplateView.as_view(template_name='signup.html')),
     url(r'^admin/', admin.site.urls),
     url(r'accounts/', include('django.contrib.auth.urls')),
-    url(r'^$', TemplateView.as_view(template_name='base.html'), name='index'),
+
+    # web site
+    url(r'^features/$', TemplateView.as_view(template_name='web/features.html'), name='features'),
+    url(r'^princing/$', TemplateView.as_view(template_name='web/princing.html'), name='princing'),
+    url(r'^about/$', TemplateView.as_view(template_name='web/about.html'), name='about'),
+    url(r'^$', TemplateView.as_view(template_name='web/index.html'), name='index'),
 ]
